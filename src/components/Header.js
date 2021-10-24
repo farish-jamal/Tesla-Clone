f946ccd71d4d1d2e0a95a5882def4778b326d873
farish <developer.websites1@gmail.com>
2021-10-24 12:59:17 +0530

    header.js

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import TeslaLogo from '../assets/logo.svg'
+import './header.css'
+import MenuIcon from '@mui/icons-material/Menu';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__logo">
+                <img src={TeslaLogo} alt="main_logo" />
+            </div>
+            <div className="header__links">
+                <p>Model s</p>
+                <p>Model 3</p>
+                <p>Model x</p>
+                <p>Model y</p>
+            </div>
+            <div className="header__right">
+                <p>Shop</p>
+                <p>Tesla Account</p>
+                 <MenuIcon />
+            </div>
+        </div>
+    )
+}
+
+export default Header
